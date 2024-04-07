5b8509df1643a96e501165089a3d6d699fa122b2
Cookie <jiahao.cai@inanov.com>
2024-04-07 20:25:44 +0800

    评价接口

--- controllers/comment/index.js
+++ controllers/comment/index.js
@@ -1,13 +1,20 @@
 "use strict";
 
 const CommentModel = require("../../models/comment");
+const UserModel = require("../../models/user");
 const formidable = require("formidable");
 const dtime = require("time-formater");
 
 class Comment{
     /* 查询老师的评论列表 */
     async list(req, res, next){
-        const {pageNum=1, pageSize=15, teaId} = req.query
+        /* 
+            TEACHER 查询对他所有的评论
+            STUDENT 查询自己所有的评论
+            ADMIN   查询教师所有的评论
+
+        */
+        const {pageNum=1, pageSize=15, id} = req.query
         const userId = req.headers.authorization;
         const errObj = {
             code: 400,
@@ -15,16 +22,29 @@ class Comment{
             message: ''
         }
 
-        if(!teaId){
-            errObj.message = '被评论者id不能为空'
-            res.send(errObj)
-        }else if(!userId){
-            errObj.message = '评论者id不能为空'
+        // if(!id){
+        //     errObj.message = '被评论者id不能为空'
+        //     res.send(errObj)
+        // }
+        if(!userId){
+            errObj.message = '用户Id不能为空'
             res.send(errObj)
         }
 
+        const USER = await UserModel.findOne({id: userId})
+        const {type} = USER
+
+        const filter = {}
+        if(type === 'TEACHER'){
+            filter.teaId = id
+        }else if(type === 'STUDENT'){
+            filter.stuId = id
+        }else{
+            filter.teaId = id
+        }
+
         // 查询教师所有的评论，越靠近当前查询时间的在列表中越靠前
-        const commentsList = await CommentModel.find({teaId}).skip((pageNum-1)*pageSize).limit(pageSize).sort({createTime: -1})
+        const commentsList = await CommentModel.find(filter).skip((pageNum-1)*pageSize).limit(pageSize).sort({createTime: -1})
         
         res.send({
             code: 0,
@@ -34,10 +54,11 @@ class Comment{
     }
 
     /* 学生发布评论 */
-    async post(){
+    async post(req, res, next){
         const form = new formidable.IncomingForm();
         form.parse(req, async (err, fields, files) => {
-            const { teaId, stuId, content, rate } = fields
+            const { teaId, content, rate } = fields
+            const stuId = req.headers.authorization
             const errObj = {
                 code: 400,
                 data: null,
@@ -59,7 +80,18 @@ class Comment{
                 res.send(errObj)
             }
 
-            const comment = await CommentModel.create({teaId, stuId, content})
+            const STUDENT = await UserModel.findOne({id: stuId}, "-password")
+            const TEACHER = await UserModel.findOne({id: teaId}, "-password")
+
+            const comment = await CommentModel.create({
+                teaId, 
+                stuId, 
+                content,
+                createTime: dtime().format('YYYY-MM-DD HH:mm:ss'),
+                rate,
+                studentDTO: STUDENT,
+                teacherDTO: TEACHER
+            })
 
             res.send({
                 code: 0,
@@ -105,4 +137,4 @@ class Comment{
     /* 不设计修改评价功能，只允许发布 */
 }
 
-module.exports = Comment;
+module.exports = new Comment();
--- models/comment/index.js
+++ models/comment/index.js
@@ -9,6 +9,7 @@ const commentSchema = new Schema({
     teaId: Number,      // 被评论者
     stuId: Number,      // 评论者
     studentDTO: Object, // 评论者信息
+    teacherDTO: Object, // 被评论者信息
     createTime: String,
     updateTime: String,
     // 限制内容不超过500字
