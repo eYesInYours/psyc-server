4d377a06cbca3fbafe1e75b43fe768c38b1575c6
ikc <eyesin_1206@163.com>
2024-04-19 19:26:26 +0800

    “fix”

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,8 @@ const admin = require("./admin");
 const classroom = require("./classroom");
 const appoint = require("./appoint");
 const order = require("./order");
-<<<<<<< HEAD
 const util = require('./util')
-=======
 const comment = require('./comment')
->>>>>>> 5b8509df1643a96e501165089a3d6d699fa122b2
 
 module.exports = (app) => {
   app.use("/user", user);
@@ -17,9 +14,6 @@ module.exports = (app) => {
   app.use("/classroom", classroom);
   app.use("/appoint", appoint);
   app.use("/order", order);
-<<<<<<< HEAD
   app.use("/util", util);
-=======
   app.use("/comment", comment);
->>>>>>> 5b8509df1643a96e501165089a3d6d699fa122b2
 };
